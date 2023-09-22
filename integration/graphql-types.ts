1c000125452ca7107f373ad859d7218a4ac27f73
Stephen Haberman <shaberman@homebound.com>
2023-09-22 18:38:16 -0500

    Codegen.

--- integration/graphql-types.ts
+++ integration/graphql-types.ts
@@ -2,7 +2,5 @@ import { dateTypePolicy } from "src/dates";
 
 export const scalarTypePolicies = {
   Author: { fields: { birthday: dateTypePolicy, deathday: dateTypePolicy } },
-  CalendarInterval: {
-    fields: { endDate: dateTypePolicy, startDate: dateTypePolicy },
-  },
+  CalendarInterval: { fields: { endDate: dateTypePolicy, startDate: dateTypePolicy } },
 };
